63d419479973826ceeae502779f6c98540b45fc0
Lee-Narae <163362488+Lee-Narae@users.noreply.github.com>
2024-05-16 15:13:35 +0900

    나래) 1:1문의 로그인 시에만 작성 가능

--- IGLOO/src/main/java/help/controller/yj/Inquiry.java
+++ IGLOO/src/main/java/help/controller/yj/Inquiry.java
@@ -3,14 +3,40 @@ package help.controller.yj;
 import common.controller.AbstractController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import member.domain.MemberVO;
 
 public class Inquiry extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		super.setRedirect(false);
-		super.setViewPage("/WEB-INF/help/inquiry.jsp");
+		
+		HttpSession session = request.getSession();
+		MemberVO loginuser = (MemberVO) session.getAttribute("loginuser");
+		
+		if(super.checkLogin(request)) {
+			
+			String userid = loginuser.getUserid();
+			request.setAttribute("userid", userid);
+			super.setRedirect(false);
+			super.setViewPage("/WEB-INF/help/inquiry.jsp");
+			
+		}
+		
+		else {
+			
+			String message = "1:1 문의는 로그인 후 가능합니다.";
+			String loc = "/IGLOO/login/login.ice";
+			
+			request.setAttribute("message", message);
+			request.setAttribute("loc", loc);
+			
+			super.setRedirect(false);
+			super.setViewPage("/WEB-INF/msg.jsp");
+		}
+		
+		
 	}
 
 }
