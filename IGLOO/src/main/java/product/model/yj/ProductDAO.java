b5c87f7bcdcdea54ec9c97d6b0e7507bb327953c
박예진 <yy6037@naver.com>
2024-05-21 18:10:29 +0900

    DAO, DAO_imple 내 브랜치에 커밋

--- IGLOO/src/main/java/product/model/yj/ProductDAO.java
+++ IGLOO/src/main/java/product/model/yj/ProductDAO.java
@@ -8,7 +8,7 @@ import product.domain.TasteVO;
 
 public interface ProductDAO {
 	
-	// == 컵 사이즈, 상세정보, 가격 알아오는 메소드 == //
+	// == 상품사진, 상품명(파인트, 쿼터...), 상세정보, 가격 알아오는 메소드 == //
 	List<ProductVO> getproductList() throws SQLException;
 	
 	// == 맛 목록을 조회해오기 == //
@@ -16,6 +16,9 @@ public interface ProductDAO {
 
 	// == 주문상세 하단 이미지 띄우기 == //
 	List<ProductVO> selectImageDetail() throws SQLException;
+
+	// 주문상세에서 상단 이미지 띄우기 == //
+	List<ProductVO> getimgList() throws SQLException;
 
 	
 
