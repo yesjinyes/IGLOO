508d16d8c4f14ecdb4300dc16e337acb7975d7c2
leejamong2 <leejamong2@naver.com>
2024-05-27 16:27:26 +0900

    작성가능한 리뷰 디비 연결중

--- IGLOO/src/main/java/member/controller/jy/AbleReview.java
+++ IGLOO/src/main/java/member/controller/jy/AbleReview.java
@@ -4,12 +4,12 @@ import common.controller.AbstractController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class ShowReview extends AbstractController {
+public class AbleReview extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		super.setViewPage("/WEB-INF/member/showReview.jsp");
+		super.setViewPage("/WEB-INF/member/ableReview.jsp");
 
 	}
 
